17cf6962c81877c7f71d85991560be9bb358cabe
ChrisJo <168246067+SkepticWo@users.noreply.github.com>
2024-06-14 13:32:02 -0400

    An encryptor,decryptor pair

---
+++ sample_decryptor.py
@@ -0,0 +1,91 @@
+import random
+from openfhe import *
+import tempfile
+import os
+
+
+datafolder = "/home/christopherjoseph/Downloads/client_server/demoData"
+
+	
+# Deserialize the crypto-context, ciphertext, pk,sk
+def deserialize(datafolder):
+
+	serType = BINARY
+
+	cc, res = DeserializeCryptoContext(datafolder + f"/cc.txt", serType)
+	if not res:
+            raise Exception(f"Could not read the cc.txt")
+        
+	ct = []
+	for i in range(10):  
+		holder, res = DeserializeCiphertext(datafolder + f"/ciphertext_{i}.txt", serType)
+		ct.append(holder)
+		if not res:
+			raise Exception(f"Could not read the ciphertext.txt")
+            
+	pk, res = DeserializePublicKey(datafolder + f"/pk.txt", serType)
+	if not res:
+		raise Exception(f"Could not read the pk.txt")
+
+	sk, res = DeserializePrivateKey(datafolder + f"/sk.txt", serType)
+	if not res:
+		raise Exception(f"Could not read the sk.txt")
+                
+	return cc,ct,pk,sk
+	
+# Compute
+def computes(cc,ct,pk):
+	
+	sum_r = cc.MakePackedPlaintext([0])
+	sum_r = cc.Encrypt(pk,sum_r)
+	for i in range(len(ct)):
+		sum_r = cc.EvalAdd(sum_r,ct[i])
+		
+	div_avg = 1/len(ct)
+	
+	return sum_r,div_avg
+	
+# Decrypt
+def decrypt(cc,sk,computed_ciphertext):
+	
+	plaintext = cc.Decrypt(sk,computed_ciphertext)
+	
+	plaintext = Plaintext.GetPackedValue(plaintext)
+	
+	plaintext = plaintext[0]
+		
+	return plaintext
+
+# Main Action
+def main_action():
+	
+	print("-------Program Start-------")
+	
+	print(" ")
+	datafolder = "/home/christopherjoseph/Downloads/client_server/demoData"
+	print(" ")
+	
+	cc,ct,pk,sk = deserialize(datafolder)
+	
+	computed_ciphertext,div_avg = computes(cc,ct,pk)
+	
+	plaintext = decrypt(cc,sk,computed_ciphertext)
+	
+	avg = plaintext*div_avg
+	
+	print(f"The average user response is {avg}.")
+	
+	print(" ")
+	
+	print("-------Program End-------")
+	
+def main():
+    global datafolder
+    with tempfile.TemporaryDirectory() as td:
+        # datafolder = td + "/" + datafolder
+        # os.mkdir(datafolder)
+        main_action()
+
+if __name__ == "__main__":
+    main()
+    
--- sample_encryptor.py
+++ sample_encryptor.py
@@ -1,58 +1,102 @@
+import random
 from openfhe import *
+import tempfile
+import os
 
-import requests
-import json
-import pandas as pd
 
-# Assuming we use an Excel file or an online json file to store user data
-url = "url"
+datafolder = "/home/christopherjoseph/Downloads/client_server/demoData"
 
-user_data = requests.get(url).text
+# Generate the cryptocontext
+def gen_cc(): 
 
-data_json = json.loads(user_data)
-
-user_df = pd.read_json(data_json)
-
-rows = len(df.axes[0])
-cols = len(df.axes[1])
-
-# Setting Cryptocontext
-
-parameters = CCParamsBFVRNS()
-parameters.SetPlaintextModulus(65537)
-parameters.SetMultiplicativeDepth(2)
-crypto_context = GenCryptoContext(parameters)
-
-# Enabling Features
-
-crypto_context.Enable(PKESchemeFeature.PKE)
-crypto_context.Enable(PKESchemeFeature.KEYSWITCH)
-crypto_context.Enable(PKESchemeFeature.LEVELEDSHE)
-
-# Generating key pair
-
-key_pair = crypto_context.KeyGen()
-
-# Generating Relinearization Key
-
-crypto_context.EvalMultKeyGen(key_pair.secretKey)
-
-# Obtaining Input
-
-user_list = df.tolist()
+	print(" ")
+	print("This program requires the subdirectory `" + datafolder + "' to exist, otherwise you will get an error writing serializations.")
+	print(" ")
+	
+	parameters = CCParamsBFVRNS()
+	parameters.SetPlaintextModulus(65537)
+	parameters.SetMultiplicativeDepth(2)
+	
+	cc = GenCryptoContext(parameters)
+	
+	cc.Enable(PKESchemeFeature.PKE)
+	cc.Enable(PKESchemeFeature.KEYSWITCH)
+	cc.Enable(PKESchemeFeature.LEVELEDSHE)
+	
+	key_pair = cc.KeyGen()
+	
+	cc.EvalMultKeyGen(key_pair.secretKey)
+	
+	return cc,key_pair
 		
+# Generate Plaintext
+def gen_plaintext():
+	
+	num_questions = 10
+	num_clients = 1
+    	
+	answer_choice = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+	answers = []
+	answer_in_answer = []
+	for i in range(num_questions):
+		answer_in_answer.append(random.choice(answer_choice))
+		answers.append(answer_in_answer)
+		answer_in_answer = []
+	return answers
+	
+# Encrypt the plain text
+def encrypt(plaintext,cc,key_pair):
+	ct_list = []
+	for i in plaintext:
+		enc_plaintext = cc.MakePackedPlaintext(i)
+		ct_list.append(cc.Encrypt(key_pair.publicKey,enc_plaintext))
+	return ct_list
+	
+# Serialize the ciphertext    
+def serialize(datafolder,cc,ciphertext,pk,sk):
+	
+	serType = BINARY
+	
+	if not SerializeToFile(datafolder + f"/cc.txt", cc, serType):
+            raise Exception(f"Error writing serialization of cc.txt")
+      	
+	for i in range(len(ciphertext)):
+			if not SerializeToFile(datafolder + f"/ciphertext_{i}.txt", ciphertext[i], serType):
+				raise Exception(f"Error writing serialization of ciphertext.txt")
+        
+	if not SerializeToFile(datafolder + f"/pk.txt", pk, serType):
+            raise Exception(f"Error writing serialization of pk.txt")
+        
+	if not SerializeToFile(datafolder + f"/sk.txt", sk, serType):
+            raise Exception(f"Error writing serialization of sk.txt")
+        
+# Main Action
+def main_action():
+	print("-------Program Start-------")
+	
+	
+	
+	cc,key_pair = gen_cc()
+	plaintext = gen_plaintext()
+	
+	print(" ")
+	print(f"Generated answers are : {plaintext}")
 
-# Encoding payload
+	ciphertext = encrypt(plaintext,cc,key_pair)
 
-plaintext1 = crypto_context.MakePackedPlaintext(user_list)
+	serialize(datafolder,cc,ciphertext,key_pair.publicKey,key_pair.secretKey)
+	print(" ")
+	print("-------Program End-------")
+ 
+def main():
+    global datafolder
+    with tempfile.TemporaryDirectory() as td:
+        # datafolder = td + "/" + datafolder
+        # os.mkdir(datafolder)
+        main_action()
 
-
-# Encryption
-
-ciphertext1 = crypto_context.Encrypt(key_pair.publicKey,plaintext1)
-
-
-
-
-
-
+if __name__ == "__main__":
+    main()
+    
+    
+    
